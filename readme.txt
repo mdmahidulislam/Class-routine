f8a59f134934ffcbcb878f873d4774ab33f44c1f
Imran-hossain <imran75442@gmail.com>
2019-10-05 11:35:25 +0900

    Created laravel auth routes with react view with mongodb
    Read the readme.txt file for any other help.

---
+++ readme.txt
@@ -0,0 +1,5 @@
+#HELPS
+
+
+#Create laravel-auth-routes with react
+https://medium.com/@panjeh/laravel-auth-routes-email-verification-reset-password-authentication-registration-routes-fb82b3337150
--- routine/config/database.php
+++ routine/config/database.php
@@ -42,6 +42,18 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options'  => [
+                'database' => 'admin' // sets the authentication database required by mongo 3
+            ]
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
@@ -62,6 +74,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
