ed04057e9daf9367ce4e77924455379afb0acaa1
Imran-hossain <imran75442@gmail.com>
2019-10-06 10:41:36 +0900

    Login
    Login

--- routine/app/User.php
+++ routine/app/User.php
@@ -3,12 +3,21 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
+
+class User extends Eloquent implements AuthenticatableContract, CanResetPasswordContract
 {
+    use AuthenticableTrait;
     use Notifiable;
+    use CanResetPassword;
+
+    protected $connection = 'mongodb';
 
     /**
      * The attributes that are mass assignable.
